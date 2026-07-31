7bf4e819e156ab347ec4caf7ebb4daf75ce024a3
ibrahim <233965906+ibrahim-kwemoi@users.noreply.github.com>
2026-07-31 16:57:25 +0300

    export all custom error classes

---
+++ src/errors/index.ts
@@ -0,0 +1,6 @@
+export * from './MpesaError.js';
+export * from './AuthenticationError.js';
+export * from './ValidationError.js';
+export * from './ApiError.js';
+export * from './NetworkError.js';
+export * from './TimeoutError.js';
