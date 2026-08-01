1c6ef86f52af9ea1d97d6b02bc43f1581de77e00
ibrahim <233965906+ibrahim-kwemoi@users.noreply.github.com>
2026-08-01 17:21:00 +0300

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,24 @@
+// Core Facade
+export { MpesaStk } from './MpesaStk.js';
+
+// Domain Types
+export * from './types/index.js';
+
+// Custom Error Classes
+export * from './errors/index.js';
+
+// Persistence Adapters
+export * from './adapters/index.js';
+
+// Event System
+export * from './events/index.js';
+
+// Daraja Client Components
+export * from './client/index.js';
+
+// Webhook Processors & Polling
+export * from './webhooks/index.js';
+export * from './polling/index.js';
+
+// Helper Utilities
+export * from './utils/index.js';
