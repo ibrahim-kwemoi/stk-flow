660a39df82b0144e800fa1fe03afbea35a0cf30a
ibrahim <233965906+ibrahim-kwemoi@users.noreply.github.com>
2026-08-01 14:09:49 +0300

    export package entry points and public API surface

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+export { MpesaStk } from './MpesaStk.js';
+export * from './types/index.js';
+export * from './errors/index.js';
+export * from './adapters/index.js';
+export * from './events/index.js';
+export * from './utils/index.js';
